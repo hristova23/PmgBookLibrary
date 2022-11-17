7eb82959be40662fd3a2c87354f95d5e23455d1f
Victoriya Hristova <victoriya.hristova04@gmail.com>
2022-11-17 18:23:06 +0200

    Fix Views

--- Library/StartUp.cs
+++ Library/StartUp.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
-var connectionString = builder.Configuration.GetConnectionString("PmgDefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
